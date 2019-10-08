372380ee30eecb2db723aa55af0bb1623784cc6e
Theodore Sase <fo9839@wayne.edu>
2019-10-07 21:08:10 -0400

    fixed links, externalized css page, and added to example posts

--- index.php
+++ index.php
@@ -4,88 +4,17 @@
 		<title> humdrum </title>
 		<meta charset="utf-8">
 	</head>
-	<style type="text/css">
-		header, nav, footer {
-			width: 100%;
-			background-color: lightblue;
-			/*color: rgba(255, 255, 255, 0.7);*/
-			color: white;
-			font-family: georgia;
-			padding: 20px;
-		}
-		header {
-			display: float;
-			float: top;
-		}
-		nav {
-			position: relative;
-			display: inline-block;
-			font-family: Verdana;
-		}
-
-		body {
-			background-color: lightblue;
-			color: #666666;
-			font-family: arial;
-		}
-		.wrapper {
-			position: center;
-			min-width:1200px;
-			width: 100%;
-		}
-		.box {
-					min-height: 1024px;
-
-			float: left;
-			border: 2px solid black;
-		}
-		.box:nth-child(1) {
-			width: 33%;
-			background-color: red;
-		}
-		.box:nth-child(2) {
-			width: 33%;
-			background-color: green;
-		}
-		.box:nth-child(3) {
-			width: 33%;
-			background-color: yellow;
-		}
-		.page {
-		
-			background-color: white;
-			color: black;
-			font-family: Verdana;
-			padding: 20px;
-		}
-		.post {
-			min-height:160px;
-			background-color: white;
-			color: black;
-			font-family: Verdana;
-			padding: 20px;
-		}
-		footer {
-			position: fixed;
-			bottom: 0px;
-			width: 100%;
-			height: 40px;
-			background-color: white;
-			color: #666666;
-			font-family: arial;
-			padding: 20px;
-		}
-	</style>
+	<link href="humdrum.css" rel="stylesheet" type="text/css" media="screen" />
 
 	<header>
 		<div>
-			<h1><i> <img src="images/banner_x.jpg"> </i></h1>
+			<h1><i> <img src="images/banner.jpg"> </i></h1>
 		</div>
 	</header>
 
 	<nav>
 
-		<a href="index.html"> <img src="images/home.jpg"> </a> &nbsp; <a href="profile.html"> <img src="images/profile.jpg"> </a> &nbsp; <a href="trending.html"> <img src="images/trending.jpg"> </a> &nbsp; <a href="notifications.html"> <img src="images/notifications.jpg"> </a> &nbsp;
+		<a href="index.php"> <img src="images/home.jpg"> </a> &nbsp; <a href="profile.html"> <img src="images/profile.jpg"> </a> &nbsp; <a href="trending.html"> <img src="images/trending.jpg"> </a> &nbsp; <a href="notifications.html"> <img src="images/notifications.jpg"> </a> &nbsp;
 		
 
 	</nav>
@@ -109,19 +38,102 @@
 			<!--Center Page - Timeline -->
 			<div class="box">
 				<div class="page">
-
+					<!--Example Posts-->
 					<div class="post">
-						<div>
+						<div class="postDiv">
 						<img src="images/ProfileTest.jpg" alt="Profile Picture." width="32" height="32">
-						<b>Manny Calavera</b> shared Humdrum by Peter Gabriel
+						<b>Manny Calavera</b> shared at 1:43 p.m.<br>
+						<b>Humdrum<br>
+						Peter Gabriel</b>
 						</div>
-						<div>
+						<div class="postDiv">
 							<iframe width=80% height=80% src="https://www.youtube.com/embed/rLOuwNfF050"></iframe>
+							</div>
+						<div class="postDiv">
+						Average Rating: 4/5
 						</div>
-					</div>
+						<div class="postDiv"> 
+						<?php
+					include "db_connect.php";
+					
+					?>
+					<form action="" method="post">
+					Comment:<br>
+					<input type="text" name="keyword">
+					<input type="submit" value="Submit">
+					</form>
+					<?php
+					?>
+						</div>
+						
+						</div>
 
+					
+					<div class="post">
+						<div class="postDiv">
+						<img src="images/ProfileTest.jpg" alt="Profile Picture." width="32" height="32">
+						<b>Manny Calavera</b> shared at 2:30 p.m.<br>
+						<b>Lucy In The Sky With Diamonds <br>
+						The Beatles</b>
+						</div>
+						<div class="postDiv">
+							<iframe width=80% height=80% src="https://www.youtube.com/embed/juilt43avXY"></iframe>
+							</div>
+						<div class="postDiv">
+						Average Rating: 5/5
+						</div>
+						
+						<div class="postDiv"> 
+						<?php
+					include "db_connect.php";
+					
+					?>
+					<form action="" method="post">
+					Comment:<br>
+					<input type="text" name="keyword">
+					<input type="submit" value="Submit">
+					</form>
+					<?php
+					?>
+						</div>
+						
+					</div>
+					
+					
+					<div class="post">
+						<div class="postDiv">
+						<img src="images/ProfileTest.jpg" alt="Profile Picture." width="32" height="32">
+						<b>Manny Calavera</b> shared at 11:32 a.m.<br>
+						<b>Weird Part of the Night<br>
+						Louis Cole</b>
+						</div>
+						<div class="postDiv">
+							<iframe width=80% height=80% src="https://www.youtube.com/embed/glgPZmSwC4M"></iframe>
+							</div>
+						<div class="postDiv">
+						Average Rating: 5/5
+						</div>
+						
+						<div class="postDiv"> 
+						<?php
+					include "db_connect.php";
+					
+					?>
+					<form action="" method="post">
+					Comment:<br>
+					<input type="text" name="keyword">
+					<input type="submit" value="Submit">
+					</form>
+					<?php
+					?>
+						</div>
+						
+					</div>
+					
+					
+					</div>
+					
 				</div>
-			</div>
 
 			<!--Right Page - Music Search -->
 			<div class="box">
