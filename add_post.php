d338e4bdcd7120a11310f9736c7ab4100a83cf22
Theodore Sase <fo9839@wayne.edu>
2019-10-27 18:01:59 -0400

    Cleanup
    formatted timeline for easy edit

--- add_post.php
+++ add_post.php
@@ -1,19 +1,17 @@
 <?php 
-					session_start();
-					include "db_connect.php";
-					
-					$new_post = $_REQUEST["post_body"];
-					$currentUser = $_SESSION["user_id"];
-					$spotifyEmbed = $_SESSION["spotify"];
-					if($new_post)
-					{
-					echo "<h2>$new_post </h2>";
-					$sql = "INSERT INTO user_posts (Content, User, Spotify) VALUES ('$new_post', '$currentUser', '$spotifyEmbed')";
-					$result = $mysqli->query($sql);
-					}
-					
-					?>
-<?php
-   header("Location: home.php");
-   exit;
+	session_start();
+	include "db_connect.php";
+	
+	$new_post = $_REQUEST["post_body"];
+	$currentUser = $_SESSION["user_id"];
+	$spotifyEmbed = $_SESSION["spotify"];
+	
+	if($new_post) {
+		echo "<h2>$new_post </h2>";
+		$sql = "INSERT INTO user_posts (Content, User, Spotify) VALUES ('$new_post', '$currentUser', '$spotifyEmbed')";
+		$result = $mysqli->query($sql);
+	}
+	
+	header("Location: home.php");
+	exit;
 ?>
--- timeline.php
+++ timeline.php
@@ -1,19 +1,23 @@
 <link href="humdrum.css" rel="stylesheet" type="text/css" media="screen" />
 <div class= "page">
 
-		<?php
+<?php
 
-			include "db_connect.php";
-		// search for keyword
-		$sql = "SELECT * FROM user_posts";
-		$result = $mysqli->query($sql);
+	include "db_connect.php";
+	
+	// search for keyword
+	
+	$sql = "SELECT * FROM user_posts";
+	$result = $mysqli->query($sql);
 
-		$user = array();
-		$content = array();
-		$spotify = array();
+	$user = array();
+	$content = array();
+	$spotify = array();
 
-		if ($result->num_rows > 0) {
+	if ($result->num_rows > 0) {
+		
 		// output data of each row
+		
 		while($row = $result->fetch_assoc()) {
 
 			array_push($user, $row["User"]);
@@ -22,62 +26,93 @@
 		}
 
 		// looping thru the results backwards
+		
 		$i=sizeof($user) - 1;
-		foreach($user as $value){
-		echo "<div class=\"post\">
-			<div class=\"postDiv\">
-			<img src=\"images/ProfileTest.jpg\" alt=\"Profile Picture.\" width=\"32\" height=\"32\">
-			". $user[$i] . "</b>
+		foreach($user as $value): ?>
+		<div class="post">
+		
+			<div class="postDiv">
+				<img src="images/ProfileTest.jpg" alt="Profile Picture." width="32" height="32">
+				<b><?=$user[$i]?> </b>
 			</div>
-			<div class=\"postDiv\">
-				". $spotify[$i] . "
-				</div>
-			<div class=\"postDiv\">
-			<div>". $content[$i] ." </div>
-			Average Rating: <i class=\"far fa-star\" data-index=\"0\"></i> <i class=\"far fa-star\" data-index=\"1\"></i> <i class=\"far fa-star\" data-index=\"2\"></i> <i class=\"far fa-star\" data-index=\"3\"></i> <i class=\"far fa-star\" data-index=\"4\"></i>
+			
+			<br>
+			
+			<div class="postDiv">
+				<?=$spotify[$i]?>
 			</div>
-<script   src=\"https://code.jquery.com/jquery-3.4.1.min.js\"   integrity=\"sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=\"   crossorigin=\"anonymous\"></script>
-<script>
-  var ratedIndex = -1;
-  $(document).ready(function(){
-	if(localStorage.getItem('ratedIndex') != null)
-		setStars(parseInt(localStorage.getItem('ratedIndex')));
-	$('.fa-star').on('click', function(){
-	  ratedIndex= parseInt($(this).data('index'));
-	  localStorage.setItem('ratedIndex', ratedIndex);
-	});
-	$('.fa-star').mouseover(function(){
-
-	  var currentIndex = parseInt($(this).data('index'));
-	  setStars(currentIndex);
-	});
-	$('.fa-star').mouseleave(function(){
-	  resetStarColors();
-	  if(ratedIndex != -1)
-		setStars(ratedIndex);
-	});
-  });
-  function setStars(max){
-	for(var i = 0; i <= ratedIndex; i++)
-		$('.fa-star:eq('+i+')').css('color', 'green');
-  }
-  function resetStarColors(){
-	$('.fa-star').css('color', 'black');
-  }
-</script>
-		</form>
+			
+			<div class="postDiv">
+				<?=$content[$i]?>
 			</div>
-		";
-
-		$i--;
-		}
-		}
-		 else {
-		echo "no results";
-		}
-		?>
-		<br><br>
-		<?php include "comment.php" ?>
+			
+			<br>
+			
+			<div class="postDiv">
+				Average Rating:
+				<i class="far fa-star" data-index="0"></i>
+				<i class="far fa-star" data-index="1"></i>
+				<i class="far fa-star" data-index="2"></i>
+				<i class="far fa-star" data-index="3"></i>
+				<i class="far fa-star" data-index="4"></i>
+			</div>
+			
+			<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous">
+			</script>
+			
+			<script>
+				var ratedIndex = -1;
+				
+				$(document).ready(function(){
+					if(localStorage.getItem('ratedIndex') != null)
+						setStars(parseInt(localStorage.getItem('ratedIndex')));
+					$('.fa-star').on('click', function(){
+						ratedIndex= parseInt($(this).data('index'));
+						localStorage.setItem('ratedIndex', ratedIndex);
+				});
+				
+				$('.fa-star').mouseover(function(){
+					var currentIndex = parseInt($(this).data('index'));
+					setStars(currentIndex);
+				});
+				
+				$('.fa-star').mouseleave(function(){
+					resetStarColors();
+					if(ratedIndex != -1)
+						setStars(ratedIndex);
+					});
+				});
+				
+				function setStars(max){
+					for(var i = 0; i <= ratedIndex; i++)
+					$('.fa-star:eq('+i+')').css('color', 'green');
+				}
+				
+				function resetStarColors(){
+					$('.fa-star').css('color', 'black');
+				}
+			</script>
+			
+			<br>
+			
+			<!-- Comment -->
+			
+			<form action="" method="post">
+				Comment:<br>
+				<input type="text" name="keyword">
+				<input type="submit" value="Submit">
+			</form>
+			
 		</div>
 		
-	</div>
+		<?php $i--;
+		
+		endforeach;
+	}
+	
+	else {
+		echo "no results";
+	}
+	?>
+
+</div>
