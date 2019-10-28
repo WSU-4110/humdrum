cdb22e1347da9285f6c5a21a365bc78a6e41432b
Sathvik Konuganti <gh8701@wayne.edu>
2019-10-27 20:23:45 -0400

    Updated trending page and displaying stars on the timeline page

--- timeline.php
+++ timeline.php
@@ -1,5 +1,7 @@
 <link href="humdrum.css" rel="stylesheet" type="text/css" media="screen" />
+<script src="https://kit.fontawesome.com/5704b8a73a.js" crossorigin="anonymous"></script>
 <div class= "page">
+
 
 		<?php
 
@@ -21,6 +23,7 @@
 			array_push($spotify, $row["Spotify"]);
 		}
 
+
 		// looping thru the results backwards
 		$i=sizeof($user) - 1;
 		foreach($user as $value){
@@ -36,35 +39,37 @@
 			<div>". $content[$i] ." </div>
 			Average Rating: <i class=\"far fa-star\" data-index=\"0\"></i> <i class=\"far fa-star\" data-index=\"1\"></i> <i class=\"far fa-star\" data-index=\"2\"></i> <i class=\"far fa-star\" data-index=\"3\"></i> <i class=\"far fa-star\" data-index=\"4\"></i>
 			</div>
-<script   src=\"https://code.jquery.com/jquery-3.4.1.min.js\"   integrity=\"sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=\"   crossorigin=\"anonymous\"></script>
-<script>
-  var ratedIndex = -1;
-  $(document).ready(function(){
-	if(localStorage.getItem('ratedIndex') != null)
-		setStars(parseInt(localStorage.getItem('ratedIndex')));
-	$('.fa-star').on('click', function(){
-	  ratedIndex= parseInt($(this).data('index'));
-	  localStorage.setItem('ratedIndex', ratedIndex);
-	});
-	$('.fa-star').mouseover(function(){
+			<script   src=\"https://code.jquery.com/jquery-3.4.1.min.js\"   integrity=\"sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=\"   crossorigin=\"anonymous\"></script>
+			<script>
+			  var ratedIndex = -1;
+			  $(document).ready(function(){
+				if(localStorage.getItem('ratedIndex') != null)
+					setStars(parseInt(localStorage.getItem('ratedIndex')));
+				$('.fa-star').on('click', function(){
+				  ratedIndex= parseInt($(this).data('index'));
+				  localStorage.setItem('ratedIndex', ratedIndex);
+				});
+				$('.fa-star').mouseover(function(){
 
-	  var currentIndex = parseInt($(this).data('index'));
-	  setStars(currentIndex);
-	});
-	$('.fa-star').mouseleave(function(){
-	  resetStarColors();
-	  if(ratedIndex != -1)
-		setStars(ratedIndex);
-	});
-  });
-  function setStars(max){
-	for(var i = 0; i <= ratedIndex; i++)
-		$('.fa-star:eq('+i+')').css('color', 'green');
-  }
-  function resetStarColors(){
-	$('.fa-star').css('color', 'black');
-  }
-</script>
+				  var currentIndex = parseInt($(this).data('index'));
+				  setStars(currentIndex);
+				});
+				$('.fa-star').mouseleave(function(){
+				  resetStarColors();
+				  if(ratedIndex != -1)
+					setStars(ratedIndex);
+				});
+			  });
+			  function setStars(max){
+				for(var i = 0; i <= ratedIndex; i++)
+					$('.fa-star:eq('+i+')').css('color', 'green');
+			  }
+			  function resetStarColors(){
+				$('.fa-star').css('color', 'black');
+			  }
+			</script>
+
+
 		</form>
 			</div>
 		";
@@ -79,5 +84,5 @@
 		<br><br>
 		<?php include "comment.php" ?>
 		</div>
-		
+
 	</div>
