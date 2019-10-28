cf3a664aafac5decb5b7b60e7eef2738c28eb265
Theodore Sase <fo9839@wayne.edu>
2019-10-28 16:47:11 -0400

    Started Profile Page
    made a template profile page. Added timeline to left column of home, which can be set to retrieve user-relavent posts.

---
+++ profile_details.php
@@ -0,0 +1,50 @@
+<link href="humdrum.css" rel="stylesheet" type="text/css" media="screen" />
+<div class= "page">
+
+	<div class= "padd">
+
+		<!-- Profile Picture -->
+		<div class= "padd">
+		<img src="images/ProfileTest.jpg" alt="Profile Picture." width="128" height="128">
+		</div>
+		
+		<!-- Current User's Name -->
+		<div class= "padd">
+			<h1>John Doe</h1>
+		</div>
+		
+	</div>
+	
+	<br>
+	
+	<!-- Information -->
+	<div class= "box_drawn">
+	
+	<h2>Stats:</h2><br>
+		Join date: 8/30/2025<br>
+		Posts: 12<br>
+		
+	</div>
+	
+	<br>
+	
+	<!-- Bio -->
+	<div class= "box_drawn">
+	
+	<h2>Bio:</h2><br>
+		Just a Wayne State computer scientist.
+		
+	</div>
+	
+	<br>
+	
+	<!-- Reccomendations -->
+	<div class= "box_drawn">
+	
+	<h2>Music Reccomendations:</h2><br>
+		MUSIC LINKS
+		
+	</div>
+
+
+</div>
