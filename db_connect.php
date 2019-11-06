5609094a017d2485f9d08edf20940d9d03d133be
Zach Pruitt <gi5840@wayne.edu>
2019-11-06 16:08:01 -0500

    Singleton design pattern applied

--- db_connect.php
+++ db_connect.php
@@ -1,10 +1,26 @@
 <?php
 
-$host = "localhost";
-$username = "root";
-$user_pass = "usbw";
-$database_in_use = "test";
 
-$mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
+	public class db_connect {
+		
+		private $host;
+		private $username;
+		private $user_pass;
+		private $database_in_use;
+		
+		public function __construct() {
+		$this->host = "localhost";
+		$this->username = "root";
+		$this->user_pass = "usbw";
+		$this->database_in_use = "test";
+		}
+
+		public function connect(){
+		
+		$mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
+		return $mysqli;
+		}
+
+	}
 
 ?>
