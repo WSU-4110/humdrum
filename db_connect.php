2ec1b332b10ae16716124ee0f86f66d73b433b79
maclo4 <fy1434@wayne.edu>
2019-11-07 15:10:37 -0500

    Created singleton class for database connection

--- db_connect.php
+++ db_connect.php
@@ -1,10 +1,18 @@
 <?php
-
-$host = "localhost";
-$username = "root";
-$user_pass = "usbw";
-$database_in_use = "test";
-
-$mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
-
+class DbConnection {
+    function __construct( $name, $age ) {
+		$mysqli = new mysqli($host, $username, $user_pass, $database_in_use);}
+	
+    static function DbConnection getDbConnection() {
+        if ($instance== null )
+            instance = new DbConnection() ;
+        return instance;    }
+    
+private static $instance = null;
+private $host = "localhost";
+private $username = "root";
+private $user_pass = "usbw";
+private $database_in_use = "test";
+private $mysqli;
+}
 ?>
