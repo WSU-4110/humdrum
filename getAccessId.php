9d0c8cd2cc5dca42b3fd0790a1978dd55169796c
maclo4 <fy1434@wayne.edu>
2019-11-07 16:40:52 -0500

    Update getAccess to use singleton pattern

--- getAccessId.php
+++ getAccessId.php
@@ -1,25 +1,33 @@
 <?php
 
-if(!isset($_SESSION))
-    {
-	session_start();
-    }
+class SpotifyConnect{
+    
 include "db_connect.php";
-
 require 'vendor/autoload.php';
+    
+private static $instance = null;
+private $accessToken = null;
 
-$session = new SpotifyWebAPI\Session(
+private $session = new SpotifyWebAPI\Session(
     'afb26d61a1f34dfd958e31def5798792',
-    '74a1595ea6974b68bc9a10e2897750d0'
-);
-
-$session->requestCredentialsToken();
-$accessToken = $session->getAccessToken();
-
-
-echo $accessToken;
-$api = new SpotifyWebAPI\SpotifyWebAPI();
-$api->setAccessToken($accessToken);
-$sql = "INSERT INTO SpotifyKey (AccessToken) VALUES ('$accessToken')";
+    '74a1595ea6974b68bc9a10e2897750d0');
+    
+    function __construct( $name, $age ) {
+		$session->requestCredentialsToken();
+        $accessToken = $session->getAccessToken();
+        
+        $api = new SpotifyWebAPI\SpotifyWebAPI();
+        $api->setAccessToken($accessToken);
+        $sql = "INSERT INTO SpotifyKey (AccessToken) VALUES ('$accessToken')";
 		$result = $mysqli->query($sql);
+}}
+	
+    static function SpotifyConnect getConnection(){
+        if ($instance== null )
+            instance = new SpotifyConnect() ;
+        return instance;  
+    
+    
+
+
 ?>
