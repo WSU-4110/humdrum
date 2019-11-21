35ab4cc213e90a563fb79750b3a7d085bdf90fff
Sathvik Konuganti <gh8701@wayne.edu>
2019-11-20 20:06:41 -0500

    updating the hashtag for profile page

--- util/profile_timeline.php
+++ util/profile_timeline.php
@@ -13,28 +13,30 @@
 	$result = $mysqli->query($sql);
 
 	$user = array();
+	$tag = array();
 	$content = array();
 	$spotify = array();
     $postid = array();
-	
-	
-	
+
+
+
 	if ($result->num_rows > 0) {
 
 		// output data of each row
 
 		while($row = $result->fetch_assoc()) {
-			if($row["User"] == $profile_user) {
+
 				array_push($user, $row["User"]);
+				array_push($tag, $row["hashtag"]);
 				array_push($content, $row["Content"]);
 				array_push($spotify, $row["Spotify"]);
 				array_push($postid, $row["PostID"]);
-			}
+
 		}
 
 		if ($user != NULL) {
 			// looping thru the results backwards ?>
-			<h2>Posts from <?=$profile_user?>:</h2>
+
 			<?php
 			$i=sizeof($user) - 1;
 			foreach($user as $value):
