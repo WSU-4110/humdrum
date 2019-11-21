d57dc552042bb47040fde666fbf57317c43b6e63
Sathvik Konuganti <gh8701@wayne.edu>
2019-11-20 20:34:06 -0500

    Fixing tredning page

--- trending.php
+++ trending.php
@@ -50,21 +50,20 @@
 	//$sql = "SELECT user, content, tag FROM hashtag WHERE tag LIKE '%" . $keywordfromform . "%'";
 	//$result = $mysqli->query($sql);
 	function convertHashtagtoLink($taghash){
-		$expression = "/#+[a-zA-Z0-9_]/";
-		$taghash = preg_replace($expression, '<a href="hashtag.php?tag=$0">$0</a>', $taghash);
+		$expression = "/#+([a-zA-Z0-9_])+/";
+		$taghash = preg_replace($expression, '<a href="hashtag.php?tag=$1">$0</a>', $taghash);
 		return $taghash;
 	}
-	$sql = "SELECT user, content, tag FROM hashtag WHERE tag LIKE '%" . $keywordfromform . "%'";
+	$sql = "SELECT User, Content, hashtag FROM user_posts WHERE hashtag LIKE '%" . $keywordfromform . "%'";
 	$result = $mysqli->query($sql);
 
-	$taghash = "SELECT tag FROM hashtag WHERE content LIKE '%" . $keywordfromform . "%'";
-	//$sql = convertHashtagtoLink($sql);
+	$taghash =  $keywordfromform;
+	$sql = convertHashtagtoLink($sql);
 	if ($result->num_rows > 0) {
 	// output data of each row
 	while($row = $result->fetch_assoc()) {
 		$taghash = convertHashtagtoLink($taghash);
-	echo "<br>" . "<b>User: </b>" . $row["user"]. "<br>" . "<b>Post: </b>" . $row["content"]. "<br>";
-	echo $taghash;
+	echo "<br>" . "<b>User: </b>" . $row["User"]. "<br>" . "<b>Post: </b>" . $row["Content"]. $taghash."<br>";
 
 	}
 	} else {
