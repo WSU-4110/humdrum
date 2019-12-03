0b3a33d2271c632610a350cf14111e5c4e8777c9
maclo4 <fy1434@wayne.edu>
2019-12-03 15:32:47 -0500

    Messed up last time

--- firstTest.php
+++ firstTest.php
@@ -4,7 +4,7 @@
 use PHPUnit\Framework\TestCase;
 //use util\SpotifyClass\spotifyResult;
 $root = $_SERVER['DOCUMENT_ROOT'];
-include $root. '/util/simpleSpotifyApp.php';
+include 'util/simpleSpotifyApp.php';
 
 
 class firstTest extends TestCase
