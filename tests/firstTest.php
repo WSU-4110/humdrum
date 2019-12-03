f38207be3582c60b1d12c0d9ebf29b9f2c73f913
maclo4 <fy1434@wayne.edu>
2019-12-03 15:07:44 -0500

    Changed backslashes to forward

--- tests/firstTest.php
+++ tests/firstTest.php
@@ -4,7 +4,7 @@
 use PHPUnit\Framework\TestCase;
 //use util\SpotifyClass\spotifyResult;
 $root = $_SERVER['DOCUMENT_ROOT'];
-include $root. '\humdrum\util\simpleSpotifyApp.php';
+include $root. '/util/simpleSpotifyApp.php';
 
 
 class firstTest extends TestCase
