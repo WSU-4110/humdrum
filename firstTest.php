d8673713c554eb0105529bd1e77631272db51609
maclo4 <fy1434@wayne.edu>
2019-12-04 01:02:16 -0500

    Added a test suite

---
+++ firstTest.php
@@ -0,0 +1,86 @@
+<?php
+
+//use humdrum\SpotitfyApp;
+
+use PHPUnit\Framework\TestCase;
+//use util\SpotifyClass\spotifyResult;
+$root = $_SERVER['DOCUMENT_ROOT'];
+//include $root. '/util/simpleSpotifyApp.php';
+include 'util\simpleSpotifyApp.php';
+
+
+class firstTest extends TestCase
+{
+    function testIfSpotifyArtistsAreSetCorrectly()
+    {
+        $artists = new SpotitfyApp\spotifyResult();
+        $artistArray = array();
+        array_push($artistArray, "billy");
+        array_push($artistArray, "eyelash");
+        $artists->setArtists($artistArray);
+        $this->assertEquals($artistArray, $artists->getArtistNames(), "Test if spotifyResult class is set correctly");
+
+    }
+
+    function testIfSpotifyAlbumsAreSetCorrectly()
+    {
+        $albums = new spotifyResult;
+        $albumArray = array();
+        array_push($albumArray, "kid a");
+        array_push($albumArray, "test album");
+        $albums->setAlbums($albumArray);
+        $this->assertEquals($albumArray, $albums->getAlbumNames(), "Test if spotifyResult class is set correctly");
+    }
+
+    function testIfSpotifyPlaylistAreSetCorrectly()
+    {
+        $playlists = new spotifyResult;
+        $playlistArray = array();
+        array_push($playlistArray, "billy");
+        array_push($playlistArray, "eyelash");
+        $playlists->setPlaylists($playlistArray);
+        $this->assertEquals($playlistArray, $playlists->getPlaylistNames(), "Test if spotifyResult class is set correctly");
+    }
+
+    function testIfSpotifyUrisAreSetCorrectly()
+    {
+        $uri = new spotifyResult;
+        $uriArray = array();
+        array_push($uriArray, "123456789");
+        array_push($uriArray, "12345");
+        $uri->setPlaylists($uriArray);
+        $this->assertEquals($uriArray, $uri->getPlaylistNames(), "Test if spotifyResult class is set correctly");
+    }
+
+    function testIfMusicTypeSessionVariableIsCorrect(){
+
+        $musicType = setMusicType("artist");
+        $this->assertEquals("artists", $musicType);
+
+        $musicType = setMusicType("playlist");
+        $this->assertEquals("playlists", $musicType);
+
+        $musicType = setMusicType("album");
+        $this->assertEquals("albums", $musicType);
+
+
+    }
+
+    function testChopIDFunction(){
+
+        $result = chopID("spotify:artist:");
+        $this->assertEquals("", $result);
+
+        $result = chopID("spotify:playlist:");
+        $this->assertEquals("", $result);
+
+        $result = chopID("spotify:album:");
+        $this->assertEquals("", $result);
+
+
+    }
+
+
+}
+
+
