e28548c4816b96fc11ad1d5f475fffeb3683bff3
sathvik88 <55303890+sathvik88@users.noreply.github.com>
2019-12-05 23:02:39 -0500

    Update trendingTest.php

--- tests/trendingTest.php
+++ tests/trendingTest.php
@@ -4,13 +4,10 @@ class trendingTest extends TestCase
 {
     public function convertTag(){
         $taghash = '<a href="hashtag.php?tag=$1">$0</a>';
-        if($taghash != '<a href="hashtag.php?tag=$1">$0</a>'){
-          return 'error';
-        }
 
     }
     public function testConversion(){
-        $this->assertSame('<a href="hashtag.php?tag=$1">$0</a>', $this->convertTag());
+        $this->assertEquals('<a href="hashtag.php?tag=$1">$0</a>', $this->convertTag());
     }
 
 }
