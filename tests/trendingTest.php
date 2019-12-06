a7529ec04c9ecc3551a9be99ae4ebf029599e17c
sathvik88 <55303890+sathvik88@users.noreply.github.com>
2019-12-05 22:57:42 -0500

    adding trendingTest again

---
+++ tests/trendingTest.php
@@ -0,0 +1,17 @@
+<?php
+use PHPUnit\Framework\TestCase;
+class trendingTest extends TestCase
+{
+    public function convertTag(){
+        $taghash = '<a href="hashtag.php?tag=$1">$0</a>';
+        if($taghas != '<a href="hashtag.php?tag=$1">$0</a>'){
+          return 'error';
+        }
+
+    }
+    public function testConversion(){
+        $this->assertSame('<a href="hashtag.php?tag=$1">$0</a>', $this->convertTag());
+    }
+
+}
+?>
